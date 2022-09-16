b93bef167518b4553ff6cafbe7c198abbd75ef22
sebas landa <slandatea@correo.unimet.edu.ve>
2022-09-16 09:38:11 -0400

    ejer 3

--- ejercicio 3.py
+++ ejercicio 3.py
@@ -7,3 +7,4 @@ year3=year2+profit
 print(f"in the first year you will earn {round (year1, 2)}")
 print(f"in the second year you will earn {round (year2, 2)}")
 print(f"in the third year you will earn {round (year3, 2)}")
+print("hello world")
