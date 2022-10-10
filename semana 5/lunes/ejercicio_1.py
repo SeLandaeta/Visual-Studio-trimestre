36a5b568059cb9a7f4113624a47e8224f9e13b9a
SeLandaeta <slandaeta@correo.unimet.edu.ve>
2022-10-10 07:46:24 -0400

    update

---
+++ semana 5/lunes/ejercicio_1.py
@@ -0,0 +1,7 @@
+#funciones lambda
+#excepciones except ValueError : print(Error ingrese otro numero)
+#solo pasa por la excepcion si existe el error nombrado
+#dentro del except puedes programar lo que tu quieras
+#modulos en phyton
+#se usa import y se printea la lista 
+#librerias de phyton
