066b9934e1369cb6ecf1e73b01bde93be2cdecb4
SeLandaeta <slandaeta@correo.unimet.edu.ve>
2022-10-24 07:32:12 -0400

    a

---
+++ Name.py
@@ -0,0 +1,13 @@
+
+    
+    
+class vehicle:
+    seller = 1
+def __init__ (self,brand,model,color,year):
+    self.brand = brand 
+    self.model = model
+    self.color = color
+    self.year = year
+
+def get_color():
+    print("El color es: ", self.color)
---
+++ Semana_7/lunes_py/ejercicio1.py
@@ -0,0 +1,12 @@
+#programacion orientada a objetos, Clase es la clasificacion de los datos en POO
+#Paradigma (instrucciones)
+#los metodos solo viven dentro de la clase a la que estan destinados.
+#atributos son las variables de un objeto
+
+class vehicle:
+    seller = 1
+def _init_ (self,brand,model,color,year):
+    self.brand = brand 
+    self.model = model
+    self.color = color
+    self.year = year
