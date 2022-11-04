e3f6c8372a82591825f9a42cf663d98fbead9b80
SeLandaeta <slandaeta@correo.unimet.edu.ve>
2022-11-04 09:36:22 -0400

    a

---
+++ ejerciciosQ2/Flores.py
@@ -0,0 +1,8 @@
+class Flore:
+    productos = []
+    def __init__(self,id,nombre,tipo,color,stock):
+        self.id = id
+        self.nombre = nombre
+        self.tipo = tipo
+        self.color = color
+        self.stock = stock
---
+++ ejerciciosQ2/mainQ2.py
@@ -0,0 +1,34 @@
+from Flores import Flore
+
+def main():
+    products = [
+    { "id": 1, "name": "Rose", "type": "flower", "stock": 160, "colors": ["red", "white", "pink"] },
+    { "id": 2, "name": "Tulip", "type": "flower", "stock": 34, "colors": ["white", "yellow"] },
+    { "id": 3, "name": "Sunflower seeds", "type": "seeds", "stock": 50 },
+    { "id": 4, "name": "Sunflower", "type": "flower", "stock": 77, "colors": ["yellow"] },
+    { "id": 5, "name": "Lavender seeds", "type": "seeds", "stock": 100, "colors": ["purple"] },
+    { "id": 6, "name": "Carnation", "type": "flower", "stock": 89, "colors": ["yellow", "burgundy", "purple", "pink", "red", "white"] },
+    ]   
+ 
+    vendor_1 = [
+    { "product_id": 5, "customer_id": 333, "amnt": 1 },
+    { "product_id": 5, "customer_id": 1010, "amnt": 2 },
+    { "product_id": 3, "customer_id": 1111, "amnt": 3 },
+    { "product_id": 2, "customer_id": 222, "amnt": 6 },
+    { "product_id": 6, "customer_id": 444, "amnt": 7 },
+    { "product_id": 1, "customer_id": 1111, "amnt": 20 },
+    ]
+    
+    vendor_2 = [
+    { "product_id": 6, "customer_id": 888, "amnt": 10 },
+    { "product_id": 1, "customer_id": 123, "amnt": 5 },
+    { "product_id": 2, "customer_id": 321, "amnt": 4 },
+    { "product_id": 4, "customer_id": 555, "amnt": 2 },
+    { "product_id": 1, "customer_id": 777, "amnt": 1 },]
+
+
+    for product in products:
+        colors = product.get("colors")
+        Flore.productos.append(Flore(product["id"],product["name"],product["type"],product["stock"],colors))
+        print (Flore.productos)
+main()
