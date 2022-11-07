77273bb349ffebb2c8ff474aa7944131b750218e
SeLandaeta <slandaeta@correo.unimet.edu.ve>
2022-11-07 07:37:22 -0400

    w

---
+++ Semana9/lunes/manejodearchivos.py
@@ -0,0 +1,9 @@
+#lectura de archivos 
+#archivo = open("archivo.txt","r")
+#datos = archivo.read()
+#archivo.close()
+#print(datos)
+#write() -> Para escribir dentro de un archivo, usar close siempre al terminar.
+#archivo.write(variable del input)
+#"with" abre y cierra el archivo automaticamente
+#lo que coloques dentro de tu with solo sera accesible dentro del mismo
