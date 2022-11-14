010fdf2776ebe7fe29ec391372b58ed8aa3cbdd2
SeLandaeta <slandaeta@correo.unimet.edu.ve>
2022-11-14 07:47:02 -0400

    a

---
+++ Semana 10/Lunes/ejercicio1.py
@@ -0,0 +1,6 @@
+#algoritmos de ordenamiento
+#bubble sort, ordenar mayores pero es muy poco recomendado
+#insertion sort 
+#selection sort más recomendado
+#merge sort se divide la lista para comparar más rápido
+#quick sort
